cdac2d3586551caa9989a4c0c7597508dd153d25
Anja Herdtfelder <anja.herdtfelder@etiqa.it>
2025-05-22 17:48:44 +0200

    feat: BE message history

--- backend/llm.py
+++ backend/llm.py
@@ -3,26 +3,32 @@ from pydantic import BaseModel
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
+from pydantic_ai.common_tools.duckduckgo import duckduckgo_search_tool
 
 ai_model_name = 'llama3.2'
-
-class CityLocation(BaseModel):
-    city: str
-    country: str
 
 
 ai_model = OpenAIModel(
     model_name=ai_model_name, provider=OpenAIProvider(base_url='http://localhost:11434/v1')
 )
-agent = Agent(ai_model, output_type=CityLocation)
+agent = Agent(ai_model, tools=[duckduckgo_search_tool()])
 
-result = agent.run_sync('Where were the olympics held in 2012?')
-print(result.output)
-#> city='London' country='United Kingdom'
-print(result.usage())
-"""
-Usage(requests=1, request_tokens=57, response_tokens=8, total_tokens=65, details=None)
-"""
-for item in result.all_messages():
+result1 = agent.run_sync('Look up the country with the highest population and then the most famous actor in the country you found using duckduckgo')
+# Very flaky, sometimes it makes one tool call and sometimes a toolcall that has two parts
+print(result1.output)
+print(result1.usage())
+
+for item in result1.all_messages():
     print()
     print(item)
+
+print()
+print()
+result2 = agent.run_sync('What are the sources for your previous answer?', message_history=result1.new_messages())
+print(result2.output)
+
+for item in result2.all_messages():
+    print()
+    print(item)
+
+# Does not work too well, it does the tool call again
