6bb3e99eab92f93bb2bfe4def9b38eb2c89593c0
Anja Herdtfelder <anja.herdtfelder@etiqa.it>
2025-05-26 12:35:18 +0200

    feat: BE add llm agent

---
+++ backend/llm_agent/llm_agent.py
@@ -0,0 +1,36 @@
+import asyncio
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
+from pydantic_ai.models.anthropic import AnthropicModel
+from pydantic_ai.providers.anthropic import AnthropicProvider
+from rich.console import Console
+from rich.live import Live
+from pydantic_ai.usage import UsageLimits
+
+ai_model_name = 'claude-3-5-sonnet-latest'
+
+def get_ai_model(model_name):
+    if model_name in ['llama3.2', 'qwen3']:
+        ai_model = OpenAIModel(
+            model_name=model_name, provider=OpenAIProvider(base_url='http://localhost:11434/v1')
+        )
+    else:
+        ai_model = AnthropicModel(
+            model_name=model_name, provider=AnthropicProvider()
+        )
+    return ai_model
+
+
+ai_model = get_ai_model(ai_model_name)
+
+agent = Agent(ai_model)
+
+
+async def stream_response():
+    console = Console()
+    with Live('', console=console, vertical_overflow='visible') as live:
+        async with agent.run_stream('Write a stroy about a lion and a tiger with no more than 1000 characters', usage_limits=UsageLimits(response_tokens_limit=1010)) as response:
+            async for message in response.stream():
+                live.update(message)
+
