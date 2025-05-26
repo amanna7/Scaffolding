598a7b23396d55bcead37a9636735f408ce4e82f
Anja Herdtfelder <anja.herdtfelder@etiqa.it>
2025-05-26 11:25:28 +0200

    feat: BE delete backend2

--- backend2/users/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from database import Base
-
-class User(Base):
-    __tablename__ = 'users'
-    
-    id = Column(String, primary_key=True, index=True)
-
--- backend2/utils/enums.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class TestEnum(str, Enum):
-    KEY1 = "key 1"
-    KEY2 = "key 2"
