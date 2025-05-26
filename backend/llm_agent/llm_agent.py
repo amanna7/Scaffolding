e8e2d2cf783700e568b3b05b02e0c500da6e186d
Anja Herdtfelder <anja.herdtfelder@etiqa.it>
2025-05-26 14:45:21 +0200

    feat: BE implement post endpoint for llm agent

--- backend/llm_agent/llm_agent.py
+++ backend/llm_agent/llm_agent.py
@@ -1,11 +1,9 @@
-import asyncio
-from pydantic_ai import Agent, RunContext
+from typing import AsyncGenerator
+from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 from pydantic_ai.models.anthropic import AnthropicModel
 from pydantic_ai.providers.anthropic import AnthropicProvider
-from rich.console import Console
-from rich.live import Live
 from pydantic_ai.usage import UsageLimits
 
 ai_model_name = 'claude-3-5-sonnet-latest'
@@ -27,10 +25,7 @@ ai_model = get_ai_model(ai_model_name)
 agent = Agent(ai_model)
 
 
-async def stream_response():
-    console = Console()
-    with Live('', console=console, vertical_overflow='visible') as live:
-        async with agent.run_stream('Write a stroy about a lion and a tiger with no more than 1000 characters', usage_limits=UsageLimits(response_tokens_limit=1010)) as response:
-            async for message in response.stream():
-                live.update(message)
-
+async def stream_response(request: str) -> AsyncGenerator[str, None]:
+    async with agent.run_stream(request, usage_limits=UsageLimits(response_tokens_limit=1010)) as response:
+        async for message in response.stream_text(delta=True):
+            yield message
--- backend/llm_agent/routers.py
+++ backend/llm_agent/routers.py
@@ -1,7 +1,12 @@
+import asyncio
 import logging
+from typing import Any, AsyncGenerator
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import StreamingResponse
 from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
+
+from .llm_agent import stream_response
 from . import schemas
 from database import get_db
 from utils.auth import validate_token, security
@@ -9,12 +14,20 @@ from utils.logger import logger
 
 router = APIRouter()
 
-@router.post("", response_model=schemas.ChatResponse,include_in_schema=False)
-@router.post("/", response_model=schemas.ChatResponse)
+@router.post("", response_model=str,include_in_schema=False)
+@router.post("/", response_model=str)
 async def get_all_users(request: schemas.ChatRequest, db: Session = Depends(get_db), token: HTTPAuthorizationCredentials = Depends(security)):
     await validate_token(token)  # Qui possiamo verificare eventualmente i permessi su token_user_id per maggiore granularità
     try:
-        return schemas.ChatResponse(response="This is a placeholder response", messages=request.message)
+        response = stream_response(request.message)
+
+        async def sse_generator(response: AsyncGenerator[str, Any]) -> AsyncGenerator[str, None]:
+            async for item in response:
+                logger.debug(f'Received response chunk: {response}')
+                await asyncio.sleep(0)
+                yield item
+
+        return StreamingResponse(content=sse_generator(response), media_type="text/event-stream")
     except Exception as e:
         logger.error(f"Error retrieving users: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error retrieving users: {str(e)}")
