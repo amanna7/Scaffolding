709d36535653df57d8eb68945ac0e22ca84a6dcb
Anja Herdtfelder <anja.herdtfelder@etiqa.it>
2025-05-27 16:30:08 +0200

    feat: FE use actual llm response in chat

--- frontend/src/Api/llm_agent.ts
+++ frontend/src/Api/llm_agent.ts
@@ -3,19 +3,19 @@ import axiosClient from './axios'
 import { Message } from '../types'
 
 
-export const askLLM = async (token: string, message: string, message_history: Message[]): Promise<Message> => {
-  const response = await axiosClient.post(`/chat/`, { message, message_history }, { ...createApiHeaders(token), responseType: 'stream' })
+export const askLLM = async (token: string, message: string): Promise<Message> => {
+  const response = await axiosClient.post(`/chat/`, { message }, { ...createApiHeaders(token), responseType: 'stream' })
   console.log('LLM response:', response.data)
   console.log("type data:", typeof response.data) // substitute with raw (see below)
   // const data = JSON.parse(response.data)
   // console.log('Parsed LLM response:', data)
   // console.log("type obj data: ", typeof data)
 
-  const raw = '{"response":"Hi"}{"response":" there!"}{"response":"More text"}'; // Working example
+  // const raw = '{"response":"Hi"}{"response":" there!"}{"response":"More text"}'; // Working example
   const regex = /{[^{}]*(".*?"\s*:\s*".*?"[^{}]*)*}/g;
 
-  const matches = raw.match(regex);
-  const messages = matches!.map(json => JSON.parse(json.trim())) // Valutare se serve il trim
+  const matches = response.data.match(regex);
+  const messages = matches!.map((json: string) => JSON.parse(json.trim())) // Valutare se serve il trim
 
-  return { content: (messages.map((message) => message.response)).join(""), role: 'assistant' }
+  return { content: (messages.map((message: { response: string }) => message.response)).join(""), role: 'assistant' }
 }
